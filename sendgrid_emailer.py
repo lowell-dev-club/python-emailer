7ea59fef6f58c62da56473ada6229bd9d679cd7f
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2019-11-07 13:35:55 -0800

    Update sendgrid_emailer.py

--- sendgrid_emailer.py
+++ sendgrid_emailer.py
@@ -3,11 +3,37 @@
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
+htmlContent = '''
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+.coolclass {
+  color: rgb(255, 175, 175);
+}
+.smallText {
+  color: #ddd
+  font-size: 7px;
+}
+</style>
+</head>
+<body>
+<h1>Hello There.</h1>
+
+<p class="coolclass">Welcome to my html file</p>
+
+<p>Thanks<br>-Lowell Dev Club</p>
+<p class="smallText>Sent using python!</p>
+<a href="https://www.lowelldev.club/">Our website!</a>
+</body>
+</html>
+'''
+
 message = Mail(
-    from_email='from_email@example.com',
-    to_emails='any_email@example.com',
+    from_email='any_email_name@domain.com',
+    to_emails='email_to_send_to@domain.com',
     subject='Sending with Twilio SendGrid is Fun',
-    html_content='<strong>and easy to do anywhere, even with Python</strong>')
+    html_content=htmlContent)
 try:
     sg = SendGridAPIClient(APIKEY)
     response = sg.send(message)
