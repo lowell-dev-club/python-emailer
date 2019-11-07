7068cc2fc1ee020ab0e753a6ffd92b29e8263625
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2019-11-07 13:13:50 -0800

    Update sendgrid_emailer.py

--- sendgrid_emailer.py
+++ sendgrid_emailer.py
@@ -6,7 +6,7 @@ from sendgrid.helpers.mail import Mail
 
 message = Mail(
     from_email='from_email@example.com',
-    to_emails='cap1jedi@gmail.com',
+    to_emails='any_email@example.com',
     subject='Sending with Twilio SendGrid is Fun',
     html_content='<strong>and easy to do anywhere, even with Python</strong>')
 try:
